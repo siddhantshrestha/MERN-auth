6ef240f5aaec75a61a9b1dc17245c5600e1eded9
Siddhant shrestha <sidstha58@gmail.com>
2023-06-24 23:38:11 +0545

    Jwt token added on http cookie

--- backend/controller/userControllers.js
+++ backend/controller/userControllers.js
@@ -1,20 +1,35 @@
 import asyncHandler from "express-async-handler"
 import User from "../model/userModel.js"
+import generateToken from "../utils/generateToken.js"
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "authuser",
-  })
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  const pass = await user.matchPassword(password)
+
+  if (user && pass) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(401)
+    throw new Error("Invalid email or password ")
+  }
 })
+
 // @desc Register a new user
 // route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
-  
 
   const userExist = await User.findOne({ email })
 
@@ -30,6 +45,7 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if (user) {
+    generateToken(res, user._id)
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -39,15 +55,20 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error("Invalid user data")
   }
-  console.log(user);
+  // console.log(user)
 })
 
 // @desc logout user
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
   res.status(200).json({
-    message: "Logout User",
+    message: "User Logout User",
   })
 })
 // @desc get user profile
