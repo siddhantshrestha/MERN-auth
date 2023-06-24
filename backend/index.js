111d026235ac2a15f0d1dd0e162567609bd43937
Siddhant shrestha <sidstha58@gmail.com>
2023-06-25 00:38:42 +0545

    authentication middleware added

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 import userRoutes from "./routes/userRoutes.js"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
@@ -6,13 +7,13 @@ import connectDB from "./config/db.js"
 
 dotenv.config()
 
-
 connectDB()
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 const port = process.env.PORT || 3000
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 const router = express.Router()
 
+// import { protect } from "../middleware/authMiddleware.js"
 import {
   authUser,
   getUserProfile,
@@ -8,10 +9,14 @@ import {
   logoutUser,
   updateUserProfile,
 } from "../controller/userControllers.js"
+import { protect } from "../middleware/authMiddleware.js"
 
-router.post('/',registerUser);
-router.post('/auth',authUser);
-router.post('/logout',logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.post("/", registerUser)
+router.post("/auth", authUser)
+router.post("/logout", logoutUser)
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
